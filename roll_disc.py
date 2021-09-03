40f4b953f81aa95549797ad3e57606a8c6655631
Ankit pratap singh <33418077+impratap@users.noreply.github.com>
2021-09-03 11:25:15 +0530

    initial commit

--- roll_disc.py
+++ roll_disc.py
@@ -1,17 +1,14 @@
 import random
 
-min_val=1
-max_val=6
-
 roll_again='yes'
 
 while roll_again == 'yes' or roll_again == 'y':
     print("Rolling the Dices")
     print("The value are :")
 
-    print(random.randint(min_val, max_val))
+    print(random.randint(1,6))
 
-    print(random.randint(min_val, max_val))
+    print(random.randint(1,6))
 
     #asking the user to roll the dice again.
     roll_again=input("Roll the Dices Again ?")
